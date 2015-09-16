15de43c6df872257e36a82ad0fa95dd54d19effd
Jason Luck <jason.luck@gmail.com>
2015-09-16 11:57:05 -0400

    Adding cert generation and signature

--- scripts/create-server.sh
+++ scripts/create-server.sh
@@ -30,12 +30,29 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
-#Determine the directory to create the CA file structure in
+#Determine the server name
 if [[ ! $1 ]]; then
 	echo "server_name is required."
 	show_help
 	exit 0;
 fi
 SERVER_NAME=$1
+
+#Create the server private key
+openssl genrsa -aes256 \
+      -out $CA_DIR/private/$SERVER_NAME.key.pem 2048
+chmod 400 $CA_DIR/private/$SERVER_NAME.key.pem
+
+#Create the server request
+openssl req -config $CA_DIR/openssl.cnf \
+      -key $CA_DIR/private/$SERVER_NAME.key.pem \
+      -new -sha256 -out $CA_DIR/csr/$SERVER_NAME.csr.pem
+      
+#Sign the certificate
+openssl ca -config $CA_DIR/openssl.cnf \
+      -extensions server_cert -days 375 -notext -md sha256 \
+      -in $CA_DIR/csr/$SERVER_NAME.csr.pem \
+      -out $CA_DIR/certs/$SERVER_NAME.cert.pem
+chmod 444 $CA_DIR/certs/$SERVER_NAME.cert.pem
 
 
