adb07d178a5eaf2f4ee3ed2b35564410e9b2bca2
jasonluck <jluck@meetveracity.com>
2017-08-24 11:06:02 -0400

    Use lower encryption level so that Oracle JVMs will support the key

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -3,11 +3,11 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 show_help ()
 {
-	echo "Usage: $(basename "$0") [-h] [-d ca_directory] server_name -- program to create a server certificate request"
+	echo "Usage: $(basename "$0") [-h] [-d ca_directory] USER_NAME -- program to create a server certificate request"
 	echo "where:"
 	echo "	-h: shows this help text"
 	echo "	-d: specify the CA root directory. Defaults to the current working directory."
-	echo "	server_name: is dns name of the server. used in the certificate file name." 
+	echo "	USER_NAME: is name of the user. used in the certificate file name." 
 
 }
 
@@ -32,30 +32,30 @@ shift $((OPTIND-1))
 
 #Determine the server name
 if [[ ! $1 ]]; then
-	echo "server_name is required."
+	echo "USER_NAME is required."
 	show_help
 	exit 0;
 fi
-SERVER_NAME=$1
+USER_NAME=$1
 
-#Create the server private key
+#Create the user private key
 openssl genrsa -aes128 \
-      -out $CA_DIR/private/$SERVER_NAME.key.pem 1024
-chmod 400 $CA_DIR/private/$SERVER_NAME.key.pem
+      -out $CA_DIR/private/$USER_NAME.key.pem 1024
+chmod 400 $CA_DIR/private/$USER_NAME.key.pem
 
 #Make the csr directory in case it doesn't exist yet
 mkdir -p $CA_DIR/csr
 
-#Create the server request
+#Create the user request
 openssl req -config $CA_DIR/openssl.cnf \
-      -key $CA_DIR/private/$SERVER_NAME.key.pem \
-      -new -sha256 -out $CA_DIR/csr/$SERVER_NAME.csr.pem
+      -key $CA_DIR/private/$USER_NAME.key.pem \
+      -new -sha256 -out $CA_DIR/csr/$USER_NAME.csr.pem
       
 #Sign the certificate
 openssl ca -config $CA_DIR/openssl.cnf \
-      -extensions server_cert -days 375 -notext -md sha256 \
-      -in $CA_DIR/csr/$SERVER_NAME.csr.pem \
-      -out $CA_DIR/certs/$SERVER_NAME.cert.pem
-chmod 444 $CA_DIR/certs/$SERVER_NAME.cert.pem
+      -extensions usr_cert -days 375 -notext -md sha256 \
+      -in $CA_DIR/csr/$USER_NAME.csr.pem \
+      -out $CA_DIR/certs/$USER_NAME.cert.pem
+chmod 444 $CA_DIR/certs/$USER_NAME.cert.pem
 
 
